d7a4176e3cc17c42c5f6e2b06ec2f62c40f3be3f
memo <memo2705@gmail.com>
2024-04-14 14:36:39 -0700

    creating a map

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -3,14 +3,31 @@ import prisma from '../lib/prisma';
 
 async function main(){
 
-    //1. Borrar registros previos
+    //1. Delete previous regs
     await Promise.all([
         prisma.productImage.deleteMany(),
         prisma.product.deleteMany(),
         prisma.category.deleteMany(),
     ]);
 
-    
+    const { categories, products } = initialData;
+
+    //Categories
+    const categoriesData = categories.map( category => ({
+        name: category
+    }))
+
+    await prisma.category.createMany({
+        data: categoriesData
+    });
+
+    const categoriesDB = await prisma.category.findMany();
+    const categoriesMap = categoriesDB.reduce( (map, category) => {
+        map[category.name.toLowerCase()] = category.id;
+
+        return map;
+    }, {} as Record<string, string>); //<string=shirt, string=categoryID>
+
     console.log('Seed Executed');
 }
 
