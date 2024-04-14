78271b0c9ab9c659e4494d6f45fc3b846705d86e
memo <memo2705@gmail.com>
2024-04-14 14:07:30 -0700

    creating seed script

--- README.md
+++ README.md
@@ -6,7 +6,8 @@
 2. Create a ```.env.template``` copy and rename to ```.env``` and change environment variables
 3. Install dependencies ```npm install```
 4. Up database ```docker compose up -d```
-5. Correr migraciones de Prisma ```npx prisma migrate dev```
-6. Run project ```npm run dev```
+5. Run Prisma migrations ```npx prisma migrate dev```
+6. Execute seed ```npm run seed```
+7. Run project ```npm run dev```
 
 ## Run in prod mode
---
+++ src/seed/seed-database.ts
@@ -0,0 +1,23 @@
+import { initialData } from "./seed";
+import prisma from '../lib/prisma';
+
+async function main(){
+
+    //1. Borrar registros previos
+    await Promise.all([
+        prisma.productImage.deleteMany(),
+        prisma.product.deleteMany(),
+        prisma.category.deleteMany(),
+    ]);
+
+    
+    console.log('Seed Executed');
+}
+
+(() => {
+
+    if(process.env.NODE_ENV === 'production') return;
+
+    main();
+
+})();
